5a8f936ddc217df1c6f30c732091b6c2492a6e5a
polroyal <polycarproyal@gmail.com>
2020-09-14 14:35:21 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'fn-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+
+
+  ngOnInit() {
+  }
+
+}
